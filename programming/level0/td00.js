5c424a7c6892e825be33267a64152d3dc43b3716
Theo Dalleau <theoferah@gmail.com>
2018-12-17 11:35:22 +0100

    td 00 - Done

---
+++ programming/level0/td00.js
@@ -0,0 +1,28 @@
+/* td00 - Hello World
+
+Le but de ce tout premier TD est de modifier le code
+dans la partie "TD Part" ci dessous, de maniÃ¨re Ã  ce
+que lorsque vous Ã©xecutez le code qu'il contient
+avec "node td00.js", le rÃ©sultat "TD00: OK"
+
+Vous travaillerez uniquement dans la "TD Part" du td,
+ne modifiez PAS la "Testing Part !". Elle contient
+une sÃ©rie de tests concernant la validitÃ© de votre TD.
+
+Ici, les tests vÃ©rifient que la valeur contenue dans
+la variable test contienne la chaÃ®ne de caractÃ¨res :
+"Hello World"
+
+*/
+
+/* TD Part */
+
+const test = 'Hello World';
+
+/* Testing Part */
+
+if (test === 'Hello World') {
+    console.log('TD00: OK');
+} else {
+    console.log('TD00: KO');
+}
