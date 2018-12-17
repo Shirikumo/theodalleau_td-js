d1bafe51d9d91a6ddc351818452811607f8a267a
Theo Dalleau <theoferah@gmail.com>
2018-12-17 17:32:05 +0100

    level 1 - td 02 - Done

--- programming/level1/td02.js
+++ programming/level1/td02.js
@@ -11,8 +11,42 @@ en chiffres romains.
 /* TD Part */
 
 function roman(decimal) {
+    var centaine = ["", "C", "CC", "CCC", "CD", "D",
+        "DC", "DCC", "DCCC", "CM"];
+    var dixaine = ["", "X", "XX", "XXX", "XL", "L",
+        "LX", "LXX", "LXXX", "XC"];
+    var unite = ["", "I", "II", "III", "IV", "V",
+        "VI", "VII", "VIII", "IX"];
 
+    var result = "";
+
+    if(decimal >= 100){
+        var count = 0;
+        while (decimal >= 100){
+            count ++;
+            decimal = decimal - 100;
+        }
+        result += centaine[count];
+    }
+    if(decimal >= 10){
+        var count = 0;
+        while (decimal >= 10){
+            count ++;
+            decimal = decimal - 10;
+        }
+        result += dixaine[count];
+    }
+    if(decimal > 0){
+        var count = 0;
+        while (decimal > 0){
+            count ++;
+            decimal = decimal - 1;
+        }
+        result += unite[count];
+    }
+    return result
 }
+
 
 /* Testing Part */
 if (roman(64) == 'LXIV' && roman(128) == 'CXXVIII' && roman(512) == 'DXII') {
