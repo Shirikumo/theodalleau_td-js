e4cb2ddc3ec5124c88a36e12ff7d904ed679695a
Theo Dalleau <theoferah@gmail.com>
2018-12-17 11:38:14 +0100

    td 01 - Done

---
+++ programming/level0/td01.js
@@ -0,0 +1,23 @@
+/* td01 - Appel de fonction
+
+Le but de ce second TD est de modifier la
+fonction td01 afin qu'elle retourne la chaĆ®ne
+de caractĆØre 'Ć‡a fonctionne'.
+
+N'hĆ©sitez pas Ć  lire les tests effectuĆ©s !
+
+*/
+
+/* TD Part */
+
+function td01() {
+    return 'Ć‡a fonctionne';
+}
+
+/* Testing Part */
+
+if (td01() === 'Ć‡a fonctionne') {
+    console.log('TD01:: OK');
+} else {
+    console.log('TD01:: KO');
+}
