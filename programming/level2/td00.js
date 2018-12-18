a4b6b766452188afd69a7defba01e901697700cf
Theo Dalleau <theoferah@gmail.com>
2018-12-18 17:04:15 +0100

    level 2 - td 00 - Done

--- programming/level2/td00.js
+++ programming/level2/td00.js
@@ -13,6 +13,16 @@ Le code doit être réalisé uniquement en ES5 pour cette partie du cours (pas d
 
 /* TD Part */
 function easySort(array) {
+    for(let i = 0; i < array.length-1; i++) {
+        for(let j = 0; j < (array.length-1-i); j++) {
+            if (array[j] > array[j+1]) {
+                let temp = array[j+1];
+                array[j+1] = array[j];
+                array[j] = temp;
+            }
+        }
+    }
+    return array;
 }
 
 /* Testing Part */
